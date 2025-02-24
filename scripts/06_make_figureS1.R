7c37d7b9cf97d6444c4368b619505b4a362a8a5a
Alec Chiono <alec.chiono@colorado.edu>
2025-02-24 15:57:10 -0700

    Update style on map and include flower photos on map figure

--- scripts/06_make_figureS1.R
+++ scripts/06_make_figureS1.R
@@ -55,7 +55,7 @@ farallons <- crop(plot_dem, extent(-123.1, -122.9, 37.65, 37.75))
 dem_colors <- colorRampPalette(c("darkgreen" , "wheat", "chocolate4"), bias=2.7) #create color scale function to color topography
 water_color <- "skyblue4"
 ecotype_color <- ifelse(site_info$ecotype=="Semelparous", "grey75", "grey25")
-ecotype_hc <- ifelse(site_info$ecotype=="Semelparous", "black", "black")
+ecotype_hc <- ifelse(site_info$ecotype=="Semelparous", "black", "grey25")
 
 # Figure out plot ratio and dimensions
 plot_ratio <- dim(dem)[1]/dim(dem)[2]*1.1
@@ -66,19 +66,42 @@ dem_2corners <- extent(plot_dem) %>% as.matrix() %>% t() %>% as.data.frame()
 dem_extent <- expand_grid(x=dem_2corners$x, y=dem_2corners$y) %>% .[c(1,2,4,3),]
 inset_poly <- map_data("state") %>% filter(region=="california")
 
+
+# Create photo panels
+plot_jpeg = function(path, add=FALSE)
+{
+  require('jpeg')
+  jpg = readJPEG(path, native=T) # read the file
+  res = dim(jpg)[2:1] # get the resolution, [x, y]
+  if (!add) # initialize an empty plot area if add==FALSE
+    plot(1,1,xlim=c(1,res[1]),ylim=c(1,res[2]),asp=1,type='n',xaxs='i',yaxs='i',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
+  rasterImage(jpg,1,1,res[1],res[2])
+}
+
+jpg("photos/plants.jpg", height=2000, width=1000)
+par(mfrow=c(2,1), mar=c(0,0,0,0))
+plot_jpeg("photos/S3.jpg")
+text(75, 1000, "b", col="white", family="Times", cex=8)
+plot_jpeg("photos/I1.jpg")
+text(50, 730, "c", col="white", family="Times", cex=9)
+dev.off()
+
 # Plot
-png("figures/figure1.png", width=plot_width, height=plot_width*plot_ratio, units="in", res=1500)
-par(bty="n", bg=water_color, mar=c(0,0,0,0), oma=c(0,0,0,0), family="Verdana")
+png("figures/figureS1.png", width=plot_width*plot_ratio, height=plot_width*plot_ratio, units="in", res=1500)
+par(mfrow=(c(1,2)))
+par(bty="n", bg=water_color, mar=c(0,0,0,0), oma=c(0,0,0,0), family="Times")
 image(plot_dem, col=dem_colors(100), axes=FALSE) #plot dem
 plot(water_bodies, col=water_color, border=water_color, add=TRUE) #plot water bodies
 plot(farallons, col=dem_colors(100)[1:10], legend=FALSE, add=TRUE) #add Farallon Islands
 points(site_info, pch=21, cex=1, bg=ecotype_color, col=ecotype_hc) #plot site points
+text(-123.05, 38.95, "a", col="black", family="Times", cex=2.75)
 text(site_info, labels=site_info$site_code, 
      col=ecotype_color, halo=TRUE, hc=ecotype_hc,
-     pos=c(2, 3, 1, 4, 2, 4), cex=2, font=2)
+     pos=c(2, 4, 3, 3, 1, 3), cex=3, font=2)
 addscalebar(plotunit="latlon", unit="metric")
-par(usr=c(min(inset_poly$long)-1, min(inset_poly$long)+40, max(inset_poly$lat)-50, max(inset_poly$lat)+1))
+par(usr=c(min(inset_poly$long)-1.9, min(inset_poly$long)+40.1, min(inset_poly$lat)-3, min(inset_poly$lat)+52))
 rect(xleft=min(inset_poly$long)-.5, xright=max(inset_poly$long)+.5, ybottom=min(inset_poly$lat)-.5, ytop=max(inset_poly$lat)+.5, col="white", border="black")
 polygon(inset_poly$long, inset_poly$lat, col="gray", border="gray")
 polygon(dem_extent$x, dem_extent$y, col=NA, border="black")
+plot_jpeg("photos/plants.jpg")
 dev.off()
